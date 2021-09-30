40bc01f071c3b4c49e584baec46ddfdb94fd4fe5
BorisStehnii <bstgniy@gmail.com>
2021-09-30 09:10:56 +0300

    Д/З

--- 1_5_2.py
+++ 1_5_2.py
@@ -1,10 +1,11 @@
-Number_1 = "0500715226"
-Number_2 = "050071522И"
-Number = [Number_1, Number_2]
+number_1 = "0500715226"
+number_2 = "050071522И"
+number_3 = "2333333333333333333"
+number = [number_1, number_2, number_3]
 i = 0
-while i < 2:
-    if Number[i].isdigit() and len(Number_2) == 10:
-        print(f"{Number[i]}: правильный формат")
+while i < len(number):
+    if number[i].isdigit() and len(number[i]) == 10:
+        print(f"{number[i]}: правильный формат")
     else:
-        print(f"{Number[i]}: Неверный формат")
+        print(f"{number[i]}: Неверный формат")
     i += 1
