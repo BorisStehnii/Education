3133e3abb24134e0d9bad87aea952c0cded7a620
BorisStehnii <bstgniy@gmail.com>
2021-10-01 10:23:43 +0300

    Практика

--- practice_1_5_2.py
+++ practice_1_5_2.py
@@ -3,6 +3,11 @@ import sys
 
 string = sys.argv
 
+if len(string) != 2:
+    print("Введите строку в формате: 'abrakodabr  RRaKK'")
+    input("Press ENTER to exit")    # удержание консольного окна
+    exit(1)
+
 string_low = string[1].lower()
 string_set = list(set(string_low))
 new_string = []
