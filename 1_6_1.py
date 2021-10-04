6a84691d28ba9ac53640df1b71d6c83f2000a109
Boris Stehnii <bstegniy@gmail.com>
2021-10-04 09:35:31 +0300

    Д/З

---
+++ 1_6_1.py
@@ -0,0 +1,9 @@
+while True:
+    string = input("Введите строку в формате 'abc abcd abc bd abcd':")
+    if len(string) == 0:
+        print("ВВЕДИТЕ СТРОКУ")
+        continue
+    break
+list_words = string.lower().split(" ")
+new_string = {key: list_words.count(key) for key in list_words}
+print(new_string)
