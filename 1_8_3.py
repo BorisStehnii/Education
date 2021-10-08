258d5cb3302d5d65522397e8c5804bab83f25de4
Boris Stehnii <bstegniy@gmail.com>
2021-10-08 22:15:39 +0300

    Д/з

---
+++ 1_8_3.py
@@ -0,0 +1,14 @@
+def make_country(oper_, *args):
+    if oper_ != "-" and oper_ != "+" and oper_ != "*":
+        exit(1)
+    elif not "".join(map(str, args)).replace("-", "").isdigit():
+        exit(1)
+
+    string_ = oper_.join(map(str, args))
+    result = eval(string_)
+    print(result)
+
+
+make_country("+", 7, -8, 9)
+make_country("-", 8, 5, 3, -10)
+make_country("*", 4, 3, -15)
