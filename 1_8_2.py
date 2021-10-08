9d2da8c0213ffeafcc66f984a0c3fe6c8341b778
Boris Stehnii <bstegniy@gmail.com>
2021-10-08 15:35:55 +0300

    Д/з

--- 1_8_2.py
+++ 1_8_2.py
@@ -1,9 +1,9 @@
-def country_dict(country, capital, dict_={}):
+def make_country(country, capital, dict_={}):
     dict_.update({country: capital})
     print(dict_)
     return dict_
 
 
-country_dict("Ukrain", "Kiyv")
-country_dict("France", "Paris")
-country_dict("Moldova", "Kishinev")
+make_country("Ukrain", "Kiyv")
+make_country("France", "Paris")
+make_country("Moldova", "Kishinev")
